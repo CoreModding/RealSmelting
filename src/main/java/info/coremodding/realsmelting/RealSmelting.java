f2762c8ad5ae1ab5ac1ba25086e73a57310da54a
Professorvennie <Prob.vennie@gmail.com>
2014-06-28 14:17:38 -0400

    added license to most classes

--- src/main/java/info/coremodding/realsmelting/RealSmelting.java
+++ src/main/java/info/coremodding/realsmelting/RealSmelting.java
@@ -1,3 +1,22 @@
+/**
+ * Copyright 2014 CoreModding under LGPLv3 or later
+ * Copyright 2014 professorvennie under LGPLv3 or later
+ *
+ * This file is part of Real Smelting.
+ *
+ * Real Smelting is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Real Smelting is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Real Smelting.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package info.coremodding.realsmelting;
 
 import net.minecraft.creativetab.CreativeTabs;
@@ -24,9 +43,6 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkRegistry;
 import cpw.mods.fml.common.registry.GameRegistry;
 
-/**
- * @author James The real smelting main mod class
- */
 
 @Mod(modid = Strings.MODID, name = Strings.name , version = Strings.version)
 
